cc13039980f0c36c2fc6f1edcf3275d62f31fbf9
Sam Cassar <sam.cassar13@gmail.com>
2025-02-28 21:23:40 +1030

    feat: adjust styles

--- styles.go
+++ styles.go
@@ -5,13 +5,13 @@ import (
 )
 
 var (
-	timeStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
-	attrStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+	timeStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("250"))
+	attrStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("250"))
 
-	debugLevelStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("250"))
-	infoLevelStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("123"))
-	warnLevelStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("220"))
-	errorLevelStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("196"))
+	debugLevelStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("255"))
+	infoLevelStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("40"))
+	warnLevelStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("226"))
+	errorLevelStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("4"))
 
 	errorAttrStyle   = errorLevelStyle.Faint(true)
 	serviceAttrStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("219"))
